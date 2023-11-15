b63c39ca2f403b1fc2eb2b75c84fb7c89687a596
Rakib <rakibhasan75089@gmail.com>
2023-11-15 10:30:56 +0600

    After fixing the quiz section

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      countDown.classList.remove("flex");
+      countDown.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
@@ -46,7 +46,8 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
+  console.log(data);
   quizData = data;
   displayQuiz(data);
 };
@@ -57,24 +58,27 @@ const displayQuiz = (data) => {
     quizContainer.innerHTML = "";
     return;
   }
-
-  data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
+  let quizHTML = ' ';
+  data.forEach((quizData, i) => {
+    console.log(quizData, i);
+    quizHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
+                                  <div class="flex items-center">
+                                    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+                                      ${i + 1}
+                                    </div>
+                                    <p class="text-gray-800 text-sm">${quizData.question}</p>
+                                  </div>
+                                  <div class="grid grid-cols-2 gap-4 mt-5">
+                                    ${displayQuizOptions(quizData.options, i)}
+                                  </div>
+                                </div>`
   });
+  //Set the concatenated HTML for all quizzes into the quizContainer
+  quizContainer.innerHTML = quizHTML;
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
